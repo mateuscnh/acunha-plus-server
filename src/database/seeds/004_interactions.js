cd5fbf8206e7cb9cfb7d3209a28256de309f9ffc
Mateus Cunha <mateus.quirino@eticasolucoes.com.br>
2022-03-01 20:44:32 -0300

    first user's initial data

---
+++ src/database/seeds/004_interactions.js
@@ -0,0 +1,7 @@
+const seedInteractionsMovies = require("../../utils/seedInteractionsMovies");
+
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex("interactions").del();
+  await knex("interactions").insert(seedInteractionsMovies);
+};
