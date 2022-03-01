e654c6b03adee4c645cb70901903b0339f79e392
Mateus Cunha <mateus.quirino@eticasolucoes.com.br>
2022-03-01 16:34:59 -0300

    refactor knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,20 @@
-// Update with your config settings.
+require("dotenv/config");
 
 module.exports = {
   development: {
     client: "pg",
-    connection: {
-      database: "acunha_plus",
-      user: "postgres",
-      password: "postgres",
+    connection: "postgres://postgres:postgres@localhost/acunha_plus",
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`,
     },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`,
+    },
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     migrations: {
       tableName: "knex_migrations",
       directory: `${__dirname}/src/database/migrations`,
