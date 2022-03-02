2755e1a06fc2011eb24995b32f0b2ebde3b4a6f9
Mateus Cunha <mateus.quirino@eticasolucoes.com.br>
2022-03-01 21:30:26 -0300

    seed interactions

--- src/database/seeds/004_interactions.js
+++ src/database/seeds/004_interactions.js
@@ -3,5 +3,5 @@ const seedInteractionsMovies = require("../../utils/seedInteractionsMovies");
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("interactions").del();
-  await knex("interactions").insert(seedInteractionsMovies);
+  await knex("interactions").insert([...seedInteractionsMovies]);
 };
